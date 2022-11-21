efec409e437f7c6720bde0041a6010258473590a
Olga <olchik-n@mail.ru>
2022-11-22 00:04:38 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+int max=a;
+int min=b;
+if(a<b){
+    max=b;
+    min=a;
+}
+else{
+    max=a;
+    min=b;
+}
+Console.WriteLine($"Min = '{min}'");
+Console.WriteLine($"Max = '{max}'");
