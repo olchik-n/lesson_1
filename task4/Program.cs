d70f0e3f4d58a664aa7e98498bcb7d8a02e3d1d9
Olga <olchik-n@mail.ru>
2022-11-22 00:35:45 +0300

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = Int32.Parse(Console.ReadLine());
+int b = Int32.Parse(Console.ReadLine());
+int c = Int32.Parse(Console.ReadLine());
+int max = a;
+if (a<b)
+{
+    max=b;
+}
+else
+{
+    max=a;
+}
+if(max<c)
+{
+    max=c;
+}
+Console.WriteLine(max);
